eca5b0a2eb542c0bbfda09fb93663500dae7dde7
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-05-13 16:30:04 -0700

    Created models, controller, and migrated

---
+++ app/controllers/solutions_controller.rb
@@ -0,0 +1,16 @@
+class SolutionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
