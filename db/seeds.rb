1d26eb61c75e2f01eef05a8b9295c7a18ea5716c
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-05-14 16:22:48 -0700

    created a seed.rb file which should add to the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,53 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create([
+# create user
+StanDaMan = User.create([
 	{name: 'StanDaMan', email: 'standaman@mail.com', password: 'Password123'}
 	])
 
-Question.create([
-	{content: "Write a function named to 'acronym' that takes a string and returns an acronym."
-		+ "\n    acronym('Syntactically awesome style sheets'); // returns 'SASS'"
-		+ "\n    acronym('Washinton State Department of Transportation') // returns 'WSDOT'"
-		+ "\n    acronym('GNU's not Unix') // returns 'GNU'"},
-	{content: "Write a function named 'factorial' that takes an integer and returns the factorial of that number."
-		+ "\n    factorial(5); // returns 5x4x3x2x1 which is 120"
-		+ "\n    factorial(0); // returns 1"},
-		+ "\n    factorial(-1); // returns undefined"},
-	{content: "Write a function named factors that takes an integer and returns an array of its non-negative factors."
-		+ "\n    factors(9); // returns [1,3,9]"
-		+ "\n    factors(360); // returns [1,2,3,4,5,6,8,9,10,12,15,18,20,24,30,36,40,45,60,72,90,120,180,360]"}
-		+ "\n    factors(0); // returns [0]"}
-	])	}
+# create tags
+easyTag = Tag.create({name: 'easy'})
+middleTag = Tag.create({name: 'middle'})
+
+# create questions
+acronym_q = Question.create(
+	{content: "Write a function named 'acronym' that takes a string and returns an acronym."
+	+ "\n    acronym('Syntactically awesome style sheets'); // returns 'SASS'"
+	+ "\n    acronym('Washinton State Department of Transportation') // returns 'WSDOT'"
+	+ "\n    acronym(\"GNU's not Unix\") // returns 'GNU'"})
+
+
+factorial_q =	{content: "Write a function named 'factorial' that takes an integer and returns the factorial of that number."
+	+ "\n    factorial(5); // returns 5x4x3x2x1 which is 120"
+	+ "\n    factorial(0); // returns 1"},
+	+ "\n    factorial(-1); // returns undefined"},
+
+
+factors_q =	{content: "Write a function named factors that takes an integer and returns an array of its non-negative factors."
+	+ "\n    factors(9); // returns [1,3,9]"
+	+ "\n    factors(360); // returns [1,2,3,4,5,6,8,9,10,12,15,18,20,24,30,36,40,45,60,72,90,120,180,360]"}
+	+ "\n    factors(0); // returns [0]"}
+
+# associate tags with questions
+acronym.tags << easyTag
+factorial.tags << easyTag
+factors.tags << middleTag
+
+# answer a question
+factors_a = Solution.create({user_id: StanDaMan.id, question_id: factors_q.id, content:
+	+ "function factors(x){"
+	+ "\n  var factorArr = [];"
+	+ "\n  var limit = Math.sqrt(x);"
+	+ "\n  for (let i = 1; i <= limit; i++){"
+	+ "\n    if (x % i === 0){"
+	+ "\n      factorArr.push(i);"
+	+ "\n      if(i != x / i){"
+	+ "\n        actorArr.push(x / i);"
+	+ "\n        }"
+	+ "\n      }"
+	+ "\n    }"
+	+ "\n    factorArr.sort(function(a,b){return a-b});"
+	+ "\n  return factorArr;"
+	+ "}"})
+
