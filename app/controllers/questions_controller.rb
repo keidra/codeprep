911934393efe97a60050087171d3b16456c7b903
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-05-17 15:08:43 -0700

    separated the actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,20 @@ class QuestionsController < ApplicationController
     @tag = Tag.find_by_name params[:name]
     render :json => @tag
   end
+
+  def easy
+  	@tag = Tag.find_by_name "easy"
+  	render = :json => @tag
+  end
+
+  def medium
+  	@tag = Tag.find_by_name "meduim"
+  	render = :json => @tag
+  end
+
+  def hard
+  	@tag = Tag.find_by_name "hard"
+  	render = :json => @tag
+  end
+
 end
