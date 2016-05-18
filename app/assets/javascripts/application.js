d1d9e53861bccda3b6e1729c096dfd34c1cffe2a
keidra <keidrabaldwin@gmail.com>
2016-05-17 18:48:14 -0700

    added gem for materialize css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery 
+//= require materialize-sprockets 
 
  $( document ).ready(function(){
    $(".button-collapse").sideNav();
